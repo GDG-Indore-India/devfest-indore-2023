178d6ad3e2ca6ceb970551372b24aa3ccd48492e
priyaljain919 <priyaljain919@gmail.com>
2023-11-01 23:55:07 +0530

    Updated padding spacing in FAQ & ticketing FAQ pages.

--- src/views/TicketsFAQ.vue
+++ src/views/TicketsFAQ.vue
@@ -2,15 +2,10 @@
   <v-main class="pt-m">
     <v-container
       fluid
-      :style="{
-        backgroundImage:
-          'url(' + require('@/assets/img/hero-side-img.svg') + ')',
-      }"
-      style="background-position: top right; background-size: 38%"
       class="bg-faq-image"
     >
       <v-row justify="center" align="center">
-        <v-col cols="12" md="10">
+        <v-col cols="12" md="10" class="pt-15">
           <p class="gdg-heading mb-0 primary-text">
             Have questions about our various tickets?
           </p>
