29bc491a203a29c1b496de3e249141e1f7645bc6
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2023-11-04 21:57:12 +0530

    Implemnted Styling for component

--- src/components/home/Timer.vue
+++ src/components/home/Timer.vue
@@ -31,3 +31,86 @@
     </v-row>
   </v-container>
 </template>
+
+
+<style scoped>
+.countdown {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-bottom: 2rem;
+}
+
+.format {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  perspective: 1000px;
+}
+
+.bloc-time {
+  text-align: center;
+  margin: 0 15px;
+}
+
+.count-title {
+  font-size: 1.2em;
+  margin-bottom: 10px;
+}
+
+.figure {
+  width: 60px;
+  height: 60px;
+  background-color: #fff;
+  border-radius: 5px;
+  font-size: 1.2em;
+  line-height: 60px;
+  font-weight: 700;
+  color: #34a853;
+  box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.2), inset 2px 4px 0 0 rgba(255, 255, 255, 0.08);
+  position: relative;
+  transform-style: preserve-3d;
+  transition: transform 0.5s;
+}
+
+.top, .top-back {
+  height: 50%;
+  overflow: hidden;
+  backface-visibility: hidden;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  position: absolute;
+  width: 100%;
+}
+
+.top {
+  z-index: 3;
+  background-color: #f7f7f7;
+  transform-origin: 50% 100%;
+  transform: rotateX(0deg);
+}
+
+.bottom {
+  z-index: 1;
+  transform-origin: 50% 0;
+  transform: rotateX(180deg);
+}
+
+span {
+  position: relative;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  color: #fff !important;
+  transform: rotateX(0deg);
+}
+
+/* Add a flipped class to apply the flipping effect */
+.flipped .figure {
+  transform: rotateX(180deg);
+}
+</style>
