9ed6d3edd5f8cc7a6b5736b1a9c6c6c37372fa1f
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2023-11-15 23:41:29 +0530

    Reduce Ytcard code

--- src/components/home/YtCards.vue
+++ src/components/home/YtCards.vue
@@ -3,25 +3,13 @@
         <h2 class="google-font">Explore Previous Devfest Livestreams</h2>
         <v-row class="main">
         <YtCard
-        class="card"
-          :videoUrl="firstVideoUrl"
-          :thumbnailUrl="firstThumbnailUrl"
-          :videoTitle="firstVideoTitle"
-          :videoDescription="firstVideoDescription"
-        />
-        <YtCard
-        class="card"
-          :videoUrl="secondVideoUrl"
-          :thumbnailUrl="secondThumbnailUrl"
-          :videoTitle="secondVideoTitle"
-          :videoDescription="secondVideoDescription"
-        />
-        <YtCard
-        class="card"
-          :videoUrl="thirdVideoUrl"
-          :thumbnailUrl="thirdThumbnailUrl"
-          :videoTitle="thirdVideoTitle"
-          :videoDescription="thirdVideoDescription"
+            class="card"
+            v-for="(video, index) in videoData"
+            :key="index"
+            :videoUrl="video.videoUrl"
+            :thumbnailUrl="video.thumbnailUrl"
+            :videoTitle="video.videoTitle"
+            :videoDescription="video.videoDescription"
         />
         </v-row>
     </v-col>
@@ -37,23 +25,29 @@ export default {
     },
     data() {
         return {
-            // First Card 
-            firstVideoUrl: "https://youtu.be/GO32M5G5mm4?si=b3Zqy26lrRMGm87R",
-            firstThumbnailUrl: "https://img.youtube.com/vi/GO32M5G5mm4/maxresdefault.jpg",
-            firstVideoTitle: "Devfest Indore 2022",
-            firstVideoDescription: "An annual decentralized tech conference hosted by Google Developer Group Indore organized for local developers to connect, learn and innovate together.",
-
-            // Second Card
-            secondVideoUrl: "https://youtu.be/MyCjsFWwNm8?si=Gj0J-w5bl9I7NVgJ",
-            secondThumbnailUrl: "https://img.youtube.com/vi/MyCjsFWwNm8/maxresdefault.jpg",
-            secondVideoTitle: "Devfest Indore 2019",
-            secondVideoDescription: "An annual decentralized tech conference hosted by Google Developer Group Indore organized for local developers to connect, learn and innovate together.",
-
-            // Third Card
-            thirdVideoUrl: "https://youtu.be/s9IO5SAazzw?si=bz55fqi6iZi7Kb51",
-            thirdThumbnailUrl: "https://img.youtube.com/vi/CKvi3sT3gU8/maxresdefault.jpg",
-            thirdVideoTitle: "Devfest Indore 2018",
-            thirdVideoDescription: "An annual decentralized tech conference hosted by Google Developer Group Indore organized for local developers to connect, learn and innovate together.",
+            videoData: [
+                // First Card
+                {
+                    videoUrl: "https://youtu.be/GO32M5G5mm4?si=b3Zqy26lrRMGm87R",
+                    thumbnailUrl: "https://img.youtube.com/vi/GO32M5G5mm4/maxresdefault.jpg",
+                    videoTitle: "Devfest Indore 2022",
+                    videoDescription: "An annual decentralized tech conference hosted by Google Developer Group Indore organized for local developers to connect, learn and innovate together.",
+                } ,
+                // Second Card
+                {
+                    videoUrl: "https://youtu.be/MyCjsFWwNm8?si=Gj0J-w5bl9I7NVgJ",
+                    thumbnailUrl: "https://img.youtube.com/vi/MyCjsFWwNm8/maxresdefault.jpg",
+                    videoTitle: "Devfest Indore 2019",
+                    videoDescription: "An annual decentralized tech conference hosted by Google Developer Group Indore organized for local developers to connect, learn and innovate together.",
+                },
+                // Third Card
+                {
+                    videoUrl: "https://youtu.be/s9IO5SAazzw?si=bz55fqi6iZi7Kb51",
+                    thumbnailUrl: "https://img.youtube.com/vi/CKvi3sT3gU8/maxresdefault.jpg",
+                    videoTitle: "Devfest Indore 2018",
+                    videoDescription: "An annual decentralized tech conference hosted by Google Developer Group Indore organized for local developers to connect, learn and innovate together.",
+                }
+            ]
         }
     }
 }
@@ -73,10 +67,7 @@ export default {
 
 @media screen and (max-width: 960px) {
     .main {
-        display: grid;
         grid-template-columns: repeat(2, minmax(0, 1fr));
-        grid-gap: 1rem;
-        margin: 0 auto;
     }
     .card {
         margin: 1rem 0;
@@ -91,10 +82,7 @@ export default {
     }
 
     .main {
-        display: grid;
         grid-template-columns: repeat(1, minmax(0, 1fr));
-        grid-gap: 1rem;
-        margin: 0 auto;
     }
 }
 </style>
