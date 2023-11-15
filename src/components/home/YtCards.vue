f1acea54ac8ea80e54fa5a33ebc874a8438836c8
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2023-11-16 00:05:09 +0530

    Reduce Ytcard css code

--- src/components/home/YtCards.vue
+++ src/components/home/YtCards.vue
@@ -1,19 +1,25 @@
 <template>
-    <v-col class="section">
-        <h2 class="google-font">Explore Previous Devfest Livestreams</h2>
-        <v-row class="main">
+    <v-container>
+      <h2 class="google-font">Explore Previous Devfest Livestreams</h2>
+      <v-row>
+        <v-col
+        v-for="(video, index) in videoData"
+        :key="index"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="4"
+      >
         <YtCard
-            class="card"
-            v-for="(video, index) in videoData"
-            :key="index"
-            :videoUrl="video.videoUrl"
-            :thumbnailUrl="video.thumbnailUrl"
-            :videoTitle="video.videoTitle"
-            :videoDescription="video.videoDescription"
+          :videoUrl="video.videoUrl"
+          :thumbnailUrl="video.thumbnailUrl"
+          :videoTitle="video.videoTitle"
+          :videoDescription="video.videoDescription"
         />
-        </v-row>
-    </v-col>
-</template>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
 
 <script source>
 import YtCard from './YtCard.vue';
@@ -54,35 +60,4 @@ export default {
 </script>
 
 <style source>
-.section {
-    margin: 2rem 0;
-}
-
-.main {
-    display: grid;
-        grid-template-columns: repeat(3, minmax(0, 1fr));
-        grid-gap: 1rem;
-        margin: 0 auto;
-}
-
-@media screen and (max-width: 960px) {
-    .main {
-        grid-template-columns: repeat(2, minmax(0, 1fr));
-    }
-    .card {
-        margin: 1rem 0;
-    }
-    
-}
-
-@media screen and (max-width: 600px) {
-    .section {
-        margin: 1rem 0;
-        width: 100vw;
-    }
-
-    .main {
-        grid-template-columns: repeat(1, minmax(0, 1fr));
-    }
-}
 </style>
